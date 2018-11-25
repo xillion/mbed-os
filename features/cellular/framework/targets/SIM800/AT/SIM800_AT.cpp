0f8bccc5404eae3afebf0b72f84860e03bfd785d
Xillion <kurbanovd.s@gmail.com>
2018-11-26 00:27:13 +0300

    Add initial support SIM800 modems

---
+++ features/cellular/framework/targets/SIM800/AT/SIM800_AT.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018, Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "SIM800_AT.h"
+#include "SIM800_AT_CellularNetwork.h"
+#include "SIM800_AT_CellularPower.h"
+
+using namespace mbed;
+using namespace events;
+
+#ifdef TARGET_SIM800C
+static const AT_CellularBase::SupportedFeature unsupported_features[] =  {
+    AT_CellularBase::AT_CGSN_WITH_TYPE,
+    AT_CellularBase::SUPPORTED_FEATURE_END_MARK
+};
+#endif
+
+SIM800_AT::SIM800_AT(EventQueue &queue) : AT_CellularDevice(queue)
+{
+#ifdef TARGET_SIM800C
+    AT_CellularBase::set_unsupported_features(unsupported_features);
+#endif
+
+
+SIM800_AT::~SIM800_AT()
+{
+}
+
+AT_CellularNetwork *SIM800_AT::open_network_impl(ATHandler &at)
+{
+    return new SIM800_AT_CellularNetwork(at);
+}
+
+AT_CellularPower *SIM800_AT::open_power_impl(ATHandler &at)
+{
+    return new SIM800_AT_CellularPower(at);
+}
